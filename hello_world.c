21ffdf7c91892411d4205d885cd3e93a3035ae79
anshif14 <101887280+anshif14@users.noreply.github.com>
2022-07-06 12:38:03 +0100

    first commit

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,6 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-
 int main() {
-    // Write C code here
-    printf("Hello world \n this is the edited code");
-    
-    return 0;
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
