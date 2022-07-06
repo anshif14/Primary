2766806480d77a35cefe9da2842181d9e330d517
anshif14 <101887280+anshif14@users.noreply.github.com>
2022-07-06 15:12:22 +0100

    Delete hello_world.c

--- hello_world.c
+++
@@ -1,9 +0,0 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-
-int main() {
-    // Write C code here
-    printf("Hello world \n this is the edited code");
-    
-    return 0;
-}
